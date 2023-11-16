6c2c3f0ad2888485738f65980f10bea1a753a388
Thalis Otávio <82677913+hendo007@users.noreply.github.com>
2023-11-15 23:31:24 -0300

    Create Games2023APIApplication.java

---
+++ APIGames2023/com.projeto.games/Games2023APIApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Games2023ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Games2023ApiApplication.class, args);
+	}
+
+}
